5f19f7f1c2502bb8626adff6ee8757a0e2e4ad3e
salek-kq <salek.muiruri@kenya-airways.com>
2026-05-18 19:49:22 +0300

    Project initialization with nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "PUEA Alumni",
+  description: "PUEA Alumni website Portal",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
